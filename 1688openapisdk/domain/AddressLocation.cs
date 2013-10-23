833bdad0287f50e47e8bb2515184c08c55db913c
zhwj184 <zhwj184@gmail.com>
2013-10-23 18:33:43 +0800

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.  On branch master  Your branch is ahead of 'origin/master' by 1 commit.
     Changes to be committed:
       (use "git reset HEAD <file>..." to unstage)
    
    	new file:   1688openapisdk/domain/AddressLocation.cs
    	new file:   1688openapisdk/request/MemberInfoRequest.cs

---
+++ 1688openapisdk/domain/AddressLocation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1688openapisdk.domain
+{
+    public class AddressLocation
+    {
+        public string address { get; set; }
+
+        public string zipcode { get; set; }
+
+        public string province { get; set; }
+
+        public string district { get; set; }
+
+        public string country { get; set; }
+
+        public string city { get; set; }
+    }
+}
---
+++ 1688openapisdk/request/MemberInfoRequest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using _1688openapisdk.response;
+using _1688openapisdk.domain;
+
+namespace _1688openapisdk.request
+{
+    class MemberInfoRequest:IAliRequest<MemberInfoResponse>
+    {
+
+        public string memberId { get; set; }
+
+        public string[] returnFields { get; set; }
+
+        public string access_token { get;set;}
+
+        string IAliRequest<MemberInfoResponse>.GetApiName()
+        {
+            return "param2/1/cn.alibaba.open/member.get/";
+        }
+
+        IDictionary<string, string> IAliRequest<MemberInfoResponse>.GetParameters()
+        {
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            parameters.Add("memberId", this.memberId);
+            if (this.returnFields != null)
+            {
+                parameters.Add("returnFields", String.Join(",", this.returnFields));
+            }
+            parameters.Add("access_token", this.access_token);
+            return parameters;
+        }
+
+        void IAliRequest<MemberInfoResponse>.Validate()
+        {
+            if (this.memberId == null)
+            {
+                throw new AliException("memberId must not be null");
+            }
+        }
+        public object GetReturnType()
+        {
+            return typeof(MemberInfo);
+        }
+    }
+}
