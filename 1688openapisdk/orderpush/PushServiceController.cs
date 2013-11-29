83954a9f78676ed811b9c9b1dc3814f4f88500cf
zhwj184 <zhwj184@gmail.com>
2013-11-29 19:01:44 +0800

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.  On branch master  Changes to be committed:    (use "git reset HEAD <file>..." to unstage)
    	modified:   orderpush/PushServiceController.cs

--- 1688openapisdk/orderpush/PushServiceController.cs
+++ 1688openapisdk/orderpush/PushServiceController.cs
@@ -13,6 +13,20 @@ using _1688openapisdk.orderpush.domain;
 
 namespace _1688openapisdk.orderpush
 {
+    /// <summary>
+    /// 
+    ///  名称	类型	说明	可选
+     /// topic	String	API发送接口名称	必选
+     /// sign	String	签名串	必选
+     /// sign_method	String	签名算法,md5 hmac，默认mds	必选
+    ///  timestamp	String	时间戳，默认yyyy-MM-dd HH:mm:ss	必选
+     /// user_id	String	卖家ID	1688没有
+     /// app_key	String	应用的appkey	必选
+     /// input_charset	String	字符编码	可选
+     /// message_id	String	消息通知Id	必选
+    ///  message_time	String	消息通知时间 YYYY-MM-DD hh:mm:ss	必选
+    ///  content	String	业务参数内容，值是json或者XML格式化串	必选
+    /// </summary>
     public class PushServiceController
     {
         /// <summary>
